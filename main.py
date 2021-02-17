d7856fd1cf1a79cf696957dabc5ee8abbc2f626f
Utkarsh Varma <uvroxx2323@gmail.com>
2021-02-17 04:53:40 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template, request
+import os
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, Length
+from flask_bootstrap import Bootstrap
+
+app = Flask(__name__)
+Bootstrap(app)
+SECRET_KEY = os.urandom(32)
+app.config['SECRET_KEY'] = SECRET_KEY
+
+
+class MyForm(FlaskForm):
+    name = StringField(label='Email:', validators=[DataRequired(), Email()])
+    password = PasswordField(label='Password', validators=[DataRequired(), Length(min=8)])
+    submit = SubmitField(label="Log In")
+
+
+@app.route("/")
+def home():
+    return render_template('index.html')
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    login_form = MyForm()
+    if login_form.validate_on_submit():
+        if login_form.name.data == "admin@email.com" and login_form.password.data == "12345678":
+            print("Success")
+            return render_template("success.html")
+        else:
+            return render_template("denied.html")
+    return render_template("login.html", form=login_form)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
